e862e3aab61ae2927acf22e9ece3cc94ab490600
tkernell <tpkernell@gmail.com>
2022-09-01 14:33:46 -0500

    first commit

---
+++ contracts/ImporterContract.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+
+import { Tellor360 } from "tellor360/contracts/Tellor360.sol";
+import { TellorFlex } from "tellorflex/contracts/TellorFlex.sol";
+import { Governance } from "polygongovernance/contracts/Governance.sol";
+import { Autopay } from "autopay/contracts/Autopay.sol";
+import { QueryDataStorage } from "autopay/contracts/QueryDataStorage.sol";
+
+pragma solidity 0.8.3;
+
+contract ImporterContract {}
